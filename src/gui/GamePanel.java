32ca4e45277f4dbb4597357713121a2c1a99bbb4
Philip Mosquera Prieto <philipmp@openmarket.com>
2016-09-15 15:14:02 +0100

    Expanded the repaint method into multiple smaller methods

--- src/gui/GamePanel.java
+++ src/gui/GamePanel.java
@@ -3,10 +3,13 @@ package gui;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Point;
 
 import javax.swing.JPanel;
 
 import board.Tile;
+import gameLogic.GameBoard;
+import pieces.ActivePiece;
 
 public class GamePanel extends JPanel {
 
@@ -22,19 +25,43 @@ public class GamePanel extends JPanel {
 			}
 		}
 	}
+
 	public GamePanel() {
 		setPreferredSize(new Dimension(WIDTH, HEIGHT));
 	}
 
 	@Override
 	public void paintComponent(Graphics g) {
+
+		drawBoard(g);
+		drawPiece(g);
+		drawGrid(g);
+
+	}
+
+	private void drawGrid(Graphics g) {
+		for (int x = 0; x < gameBoard.length; x++) {
+			for (int y = 0; y < gameBoard[x].length; y++) {
+				g.setColor(Color.BLACK);
+				g.drawRect(x * scale, y * scale, scale, scale);
+			}
+		}
+	}
+
+	private void drawBoard(Graphics g) {
 		for (int x = 0; x < gameBoard.length; x++) {
 			for (int y = 0; y < gameBoard[x].length; y++) {
 				g.setColor(gameBoard[x][y].getCurrentColor());
 				g.fillRect(x * scale, y * scale, scale, scale);
-				g.setColor(Color.BLACK);
-				g.drawRect(x * scale, y * scale, scale, scale);
 			}
+		}
+	}
+
+	private void drawPiece(Graphics g) {
+		ActivePiece currentPiece = GameBoard.getActivePiece();
+		g.setColor(currentPiece.getColor());
+		for (Point point : currentPiece.getSquares()) {
+			g.fillRect(((int) point.getX()) * scale, ((int) point.getY()) * scale, scale, scale);
 		}
 	}
 
