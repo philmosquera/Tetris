500c3ec60c18bd49cfe37ac0e2543361526b839f
Philip Mosquera Prieto <philipmp@openmarket.com>
2016-09-15 15:03:13 +0100

    Added getActivePiece method

--- src/pieces/ActivePiece.java
+++ src/pieces/ActivePiece.java
@@ -1,16 +1,26 @@
 package pieces;
 
 import java.awt.Color;
+import java.awt.Point;
 import java.awt.geom.Point2D;
 import java.util.Vector;
 
 public class ActivePiece {
 
-	private Vector<Point2D> squares = new Vector<Point2D>(4);
+	private Vector<Point> squares = new Vector<Point>(4);
 	private Color pieceColor = Color.red;
 
-
 	protected ActivePiece(int nextPiece) {
+
+		squares.add(new Point(5, 10));
+
 	}
 
+	public Vector<Point> getSquares() {
+		return squares;
+	}
+
+	public Color getColor() {
+		return pieceColor;
+	}
 }
