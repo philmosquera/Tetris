ce00495a6b0511a1fd8e254b1eb4c950c172a5c9
Philip Mosquera Prieto <philipmp@openmarket.com>
2016-09-16 15:25:57 +0100

    Changed the drawing method to hide the top two squares

--- src/gui/GamePanel.java
+++ src/gui/GamePanel.java
@@ -13,18 +13,9 @@ import pieces.ActivePiece;
 
 public class GamePanel extends JPanel {
 
-	private static final int WIDTH = 300;
-	private static final int HEIGHT = 600;
+	private static final int WIDTH = 400;
+	private static final int HEIGHT = 700;
 	private static int scale = 30;
-
-	private static Tile[][] gameBoard = new Tile[10][20];
-	static {
-		for (int x = 0; x < gameBoard.length; x++) {
-			for (int y = 0; y < gameBoard[x].length; y++) {
-				gameBoard[x][y] = new Tile();
-			}
-		}
-	}
 
 	public GamePanel() {
 		setPreferredSize(new Dimension(WIDTH, HEIGHT));
@@ -32,27 +23,28 @@ public class GamePanel extends JPanel {
 
 	@Override
 	public void paintComponent(Graphics g) {
+		Tile[][] gameBoard = GameBoard.getBoard();
 
-		drawBoard(g);
+		drawBoard(g, gameBoard);
 		drawPiece(g);
-		drawGrid(g);
+		drawGrid(g, gameBoard);
 
 	}
 
-	private void drawGrid(Graphics g) {
+	private void drawGrid(Graphics g, Tile[][] gameBoard) {
 		for (int x = 0; x < gameBoard.length; x++) {
 			for (int y = 0; y < gameBoard[x].length; y++) {
 				g.setColor(Color.BLACK);
-				g.drawRect(x * scale, y * scale, scale, scale);
+				g.drawRect(x * scale, (y-2) * scale, scale, scale);
 			}
 		}
 	}
 
-	private void drawBoard(Graphics g) {
+	private void drawBoard(Graphics g, Tile[][] gameBoard) {
 		for (int x = 0; x < gameBoard.length; x++) {
 			for (int y = 0; y < gameBoard[x].length; y++) {
 				g.setColor(gameBoard[x][y].getCurrentColor());
-				g.fillRect(x * scale, y * scale, scale, scale);
+				g.fillRect(x * scale, (y-2) * scale, scale, scale);
 			}
 		}
 	}
@@ -61,7 +53,7 @@ public class GamePanel extends JPanel {
 		ActivePiece currentPiece = GameBoard.getActivePiece();
 		g.setColor(currentPiece.getColor());
 		for (Point point : currentPiece.getSquares()) {
-			g.fillRect(((int) point.getX()) * scale, ((int) point.getY()) * scale, scale, scale);
+			g.fillRect(((int) point.getX()) * scale, ((int) point.getY() - 2) * scale, scale, scale);
 		}
 	}
 
